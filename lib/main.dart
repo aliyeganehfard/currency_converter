58a6dc74804792e7a5e2fd19f8c4cef3dfac51db
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-11-02 21:36:35 +0330

    add CurrencyConverterMaterialPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter/CurrencyConverterMaterialPage.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return const MaterialApp(
-      home: Text('hello world'),
+      home: CurrencyConverterMaterialPage()
     );
   }
 
