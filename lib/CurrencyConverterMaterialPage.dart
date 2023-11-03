de3fd695ea1ebfcd24fc26f14898eee1e3c428b1
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-11-03 20:13:00 +0330

    add color and ...

--- lib/CurrencyConverterMaterialPage.dart
+++ lib/CurrencyConverterMaterialPage.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget{
+class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text('hello currency converter page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255)
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
