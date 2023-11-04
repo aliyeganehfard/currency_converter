f20b0136959d91c77f3b09943343d253b421fecf
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-11-04 20:13:38 +0330

    add text box and ...

--- lib/CurrencyConverterMaterialPage.dart
+++ lib/CurrencyConverterMaterialPage.dart
@@ -5,6 +5,18 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        width: 2,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(5),
+      ),
+    );
+
     return const Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
@@ -14,11 +26,31 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
             Text(
               '0',
               style: TextStyle(
-                fontSize: 55,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 255, 255, 255)
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            TextField(
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black54,
               ),
-            )
+              decoration: InputDecoration(
+                  hintText: 'please enter amount in USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black54,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.monetization_on,
+                    color: Colors.green,
+                    size: 25,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+              ),
+            ),
           ],
         ),
       ),
